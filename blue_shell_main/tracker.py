544a1c5e51fe22e659e7b3348598ec08e036587c
Sang Hun Chou <shjchou@Sangs-MacBook-Pro.local>
2022-05-12 11:39:39 -0700

    Initial Deploy

--- blue_shell_main/tracker.py
+++ blue_shell_main/tracker.py
@@ -23,10 +23,13 @@ def fetch_product(url):
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=chrome_options)
     else:
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN", 'chromedriver')
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument('--window-size=1920,1080')
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--allow-running-insecure-content')
         user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
         chrome_options.add_argument(f"user-agent={user_agent}")
         # driver = webdriver.Chrome(executable_path=chrome_driver_path, chrome_options=chrome_options)
