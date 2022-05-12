182ea3f03fa1857f1c15533fc581ce1e11c334ee
Sang Hun Chou <shjchou@Sangs-MacBook-Pro.local>
2022-05-12 03:15:31 -0700

    Initial Deploy

--- blue_shell_main/tracker.py
+++ blue_shell_main/tracker.py
@@ -1,22 +1,31 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from blue_shell_main import db
+from blue_shell_main import db, ENV
 from blue_shell_main.models import Product
 
 def fetch_product(url):
-    chrome_options = Options()
-    chrome_options.headless = True
-    chrome_options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-    chrome_options.add_argument('--window-size=1920,1080')
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--allow-running-insecure-content')
-    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
-    chrome_options.add_argument(f"user-agent={user_agent}")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=chrome_options)
+    if ENV == 'dev':
+        chrome_options = Options()
+        chrome_options.headless = True
+        chrome_options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+        chrome_options.add_argument('--window-size=1920,1080')
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--allow-running-insecure-content')
+        user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
+        chrome_options.add_argument(f"user-agent={user_agent}")
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=chrome_options)
+    else:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     # Retrieve ASIN
     start = url.find('/dp/')
