51d701b8ac85bab0c58256132d891b9afa58a2ca
Sang Hun Chou <shjchou@Sangs-MacBook-Pro.local>
2022-05-12 16:45:44 -0700

    Initial Deploy

--- blue_shell_main/tracker.py
+++ blue_shell_main/tracker.py
@@ -23,7 +23,7 @@ def fetch_product(url):
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=chrome_options)
     else:
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN", 'chromedriver')
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_SHIM", None)
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
