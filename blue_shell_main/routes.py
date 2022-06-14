10bdff42506c76e4a569a57fcf438e12ca0baa2b
shjchou92 <shjchou92@gmail.com>
2022-06-13 19:01:17 -0700

    Initial Deploy

--- blue_shell_main/routes.py
+++ blue_shell_main/routes.py
@@ -1,5 +1,4 @@
-import os, psycopg2
-import urllib.parse as urlparse
+import os
 from datetime import datetime
 from flask import redirect, render_template, flash, url_for, request
 from blue_shell_main import app, db, ENV
